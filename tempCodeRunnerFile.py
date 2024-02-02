4027590b79e0e969f4bc23d83f919d3b25554a07
EktaBhaskar <40736495+EktaBhaskar@users.noreply.github.com>
2024-02-02 14:08:39 -0500

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-generate_fake_data_time = timeit.timeit(microbiology_runner.generate_fake_data(1, 10000), number=1)
-    # print("Generate_fake_data Execution Time:", generate_fake_data_time, "seconds")
